e129332e43788bd6feb4365f6c38accedd85bd3c
Shoshi <shoshii4210@gmail.com>
2017-10-01 14:50:36 +0300

    3

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 app.listen(3001, function () {
     console.log('Example app listening on port 3001!');
 });
@@ -9,5 +10,5 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 app.get('/:name?', function (req, res) {
-    res.send('Hello '+req.params.name+'!');
+    res.render('views/firstpage')
 });
